1fdd1976dcf01eedbc0c4c295c43a5ce778bbd68
Luke Jones <luke@Mac.localdomain>
2025-06-06 11:00:13 +0100

    notes in routing for later

--- resources/views/partials/routes/weapon.blade.php
+++ resources/views/partials/routes/weapon.blade.php
@@ -1,5 +1,7 @@
+{{-- To be used for things like routing in the case of tax term = bombs or something --}}
 @include('partials.content-section-classification')
 @include('partials.content-section-crew')
 @include('partials.content-section-relations')
 @include('partials.content-section-armaments')
 @include('partials.content-section-systems')
+
